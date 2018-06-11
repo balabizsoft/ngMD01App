2cc58f50b360b590545e2dc0de8638516f57ed93
bala <bala.bizsoft@gmail.com>
2018-06-11 14:52:30 +0530

    c1 Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule,MatIconModule} from '@angular/material';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { C1Component } from './c1/c1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,    
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
